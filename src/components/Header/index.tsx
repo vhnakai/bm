5f6152516da4621edfaa57dd850e3f7ea3e087c2
Vitor Hugo Nakai <36956647+vhnakai@users.noreply.github.com>
2021-05-17 13:12:06 -0300

    Refinamento da Aplicação

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,31 +1,22 @@
 import Link from "next/link";
-import { useState } from "react";
-import { FaBars, FaTimes } from "react-icons/fa";
 import styles from "./styles.module.scss";
 
 export function Header() {
-  const [click, setClick] = useState(false);
-
-  const handleClick = () => setClick(!click);
-  const closeMobileMenu = () => setClick(false);
-
   return (
     <header className={styles.container}>
       <div className={styles.content}>
-        <Link href="/" passHref>
-          <a className={styles.navbarLogo}>
-            <img src="/images/website_logo.png" alt="Beyond Money" />
-          </a>
-        </Link>
-        <div className={styles.bar} onClick={handleClick}>
-          {click ? <FaTimes /> : <FaBars />}
+        <div className={styles.logo}>
+          <Link href="/" passHref>
+            <a>
+              <img src="/images/website_logo.png" alt="Beyond Money" />
+            </a>
+          </Link>
         </div>
-        <nav className={click ? styles.active : styles.menu}>
-          <div className={styles.item} onClick={closeMobileMenu}>
-            <Link href="/about" passHref>
-              <a>Sobre</a>
-            </Link>
-          </div>
+
+        <nav>
+          <Link href="/about" passHref>
+            <a>Sobre</a>
+          </Link>
         </nav>
       </div>
     </header>
--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,14 +1,12 @@
 import { Header } from "../Header";
 import { Footer } from "../Footer";
 
-import styles from "./styles.module.scss";
-
 export function Layout({ children }) {
   return (
-    <div className={styles.container}>
+    <>
       <Header />
       {children}
       <Footer />
-    </div>
+    </>
   );
 }
